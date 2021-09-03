05c622a15cec0a13b504d8b51b7411f63e7937d2
honkwok2021 <87738305+honkwok2021@users.noreply.github.com>
2021-09-04 01:05:12 +1000

    Add list of member for startup

--- SimpleMembershipSystem/Data/DbInitializer.cs
+++ SimpleMembershipSystem/Data/DbInitializer.cs
@@ -16,6 +16,12 @@ namespace SimpleMembershipSystem.Data
                 return;
             }
 
+            List<Member> memberList = new List<Member>
+            {
+                new Member{FirstName="Peter", LastName="Green", DateOfBirth=DateTime.Parse("04-12-2010"), PhoneNumber="042766778", Email="test1111@example.com"},
+                new Member{FirstName="Jane", LastName="Lee", DateOfBirth=DateTime.Parse("04-11-1997"), PhoneNumber="04234567009", Email="2222211@example.com"},
+            };
+
             var members = new Member[]
             {
                 new Member{FirstName="John", LastName="Smith", DateOfBirth=DateTime.Parse("01-05-2000"), PhoneNumber="0423456778", Email="test@example.com"},
@@ -25,6 +31,10 @@ namespace SimpleMembershipSystem.Data
             foreach (Member m in members)
             {
                 context.Members.Add(m);
+            }
+            foreach (Member l in memberList)
+            {
+                context.Members.Add(l);
             }
             context.SaveChanges();
         }
